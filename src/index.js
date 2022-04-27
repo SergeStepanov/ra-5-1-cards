4c8d44d95841f7edaf8b380da22ca5f64bfdb7db
Sergey Stepanov <ser1653285@yandex.ru>
2022-04-28 02:03:02 +0300

    fix b

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-// import './index.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import { createRoot } from 'react-dom/client';
 
